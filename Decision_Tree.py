123e6fbdfa2f5463d965cc8cf01c816d53b2752c
A7mad7sn <104373773+A7mad7sn@users.noreply.github.com>
2022-05-30 20:18:02 +0200

    Add files via upload

--- Decision_Tree.py
+++ Decision_Tree.py
@@ -3,21 +3,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 
-def Decision_Tree(loan_Df,features):
+def Decision_Tree(Data):
     print ('------------------------------------------------------------------------')
     print('Using Decision Tree Algorithm :-')
     print('---------------------------------')
-    x = loan_Df[["Gender","Married","Dependents","Education","Self_Employed","ApplicantIncome","CoapplicantIncome","LoanAmount","Loan_Amount_Term","Credit_History","Property_Area"]].values
-    y = loan_Df["Loan_Status"].values
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y)
+    x_train, x_test, y_train, y_test = Data
 
-    loan_Model = DecisionTreeClassifier()
+    loan_Model = DecisionTreeClassifier(max_depth=6,random_state=44)
     loan_Model.fit(x_train, y_train)
     
     y_predict = loan_Model.predict(x_test)
     print('ID3 accuracy for Loan Prediction = ', metrics.accuracy_score(y_predict,y_test))
-    y_predict = loan_Model.predict(features)
     print("Prediction for Loan : ",y_predict)
     print ('------------------------------------------------------------------------')
    
--- SVM.py
+++ SVM.py
@@ -3,25 +3,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn import metrics
 
-def svm_prediction(loan_Df,features):
+def svm_prediction(Data):
     print ('--------------------------------------------------------------------------')
     print ('Using SVM :-')
     print ('-------------')
-    #Variables and Features :-
-    x = loan_Df[["Gender","Married","Dependents","Education","Self_Employed","ApplicantIncome","CoapplicantIncome","LoanAmount","Loan_Amount_Term","Credit_History","Property_Area"]].values
-    y = loan_Df["Loan_Status"].values
+   
 
     #Data Splitting :-
-    x_train, x_test, y_train, y_test = train_test_split(x, y)
+    x_train, x_test, y_train, y_test = Data
 
     #Data Training :-
-    loan_model_svm = SVC()
+    loan_model_svm = SVC(kernel = 'linear')
     loan_model_svm.fit(x_train,y_train)
 
     #Prediction for printing accuracy :-
     y_pred = loan_model_svm.predict(x_test)
     print('SVM accuracy for Loan Prediction = ',metrics.accuracy_score(y_pred,y_test))
-    y_pred = loan_model_svm.predict(features)
     print("Prediction for Loan : ",y_pred)
     print ('--------------------------------------------------------------------------')
 
